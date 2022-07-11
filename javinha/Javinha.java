74711d98eb8e630582894cda3f13d1495f539180
Rosana <rosanatsf@gmail.com>
2022-07-11 15:16:28 -0300

    Experimentos em Java

--- javinha/Javinha.java
+++ javinha/Javinha.java
@@ -2,6 +2,6 @@ public class Javinha {
     public static void main(String args[]) {
         int a = 2;
         int b = 11;
-        System.out.println("Hello World! " + (a+b));
+        System.out.println("Hello World, estou gostando muito do Javinha! " + (a+b));
     }
 }
---
+++ neko/src/cat/neko/feline.java
@@ -0,0 +1,67 @@
+package cat.neko;
+
+import java.util.Objects;
+
+public class feline {
+
+	private String nome;
+	private String cor;
+	private Integer idade;
+
+	public feline() {
+	}
+
+	public feline(String nome, String cor, Integer idade) {
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cor, idade, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		feline other = (feline) obj;
+		return Objects.equals(cor, other.cor) && Objects.equals(idade, other.idade) && Objects.equals(nome, other.nome);
+	}
+
+	@Override
+	public String toString() {
+		return "feline [nome=" + nome + ", cor=" + cor + ", idade=" + idade + "]";
+		System.out.println(cat.neko);
+	}
+
+}
