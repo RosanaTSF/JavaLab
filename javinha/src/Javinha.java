0ed1acd2c97b7b90149a0e22db867746d21cf688
Rosana <rosanatsf@gmail.com>
2022-07-16 13:18:28 -0300

    removido codigo

--- javinha/src/Javinha.java
+++
@@ -1,7 +0,0 @@
-public class Javinha {
-    public static void main(String args[]) {
-        int a = 2;
-        int b = 11;
-        System.out.println("Hello World, estou gostando muito do Javinha! " + (a+b));
-    }
-}
